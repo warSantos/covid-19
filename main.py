ddd9b90795c590b58b203fd875165ff534f09d2c
Júlio Resende <julio.cmdr@gmail.com>
2020-12-01 14:32:18 -0300

    plotando predição média

--- main.py
+++ main.py
@@ -51,8 +51,8 @@ if __name__=='__main__':
     # Treinando o algoritimo.
     graph = Graph(vertexes, edges, cities_df, cities_new_cases)
     
-    n_steps=80
-    city = 3119708  #Coronel Xavier Chaves
+    n_steps=105
+    city = 3168804  # Tiradentes
     accumulated_curve = []
 
     for i in range(len(cities_new_cases[city])):
@@ -64,25 +64,33 @@ if __name__=='__main__':
     ag = Ag(graph, accumulated_curve[0:n_steps], city)
 
     # executa o algoritmo genético
-    c, weights = ag.run(npop=30, nger=100, cp=0.9, mp=0.01, xmaxc=3.0, xmax_edge=50)
+    c, weights = ag.run(npop=30, nger=100, cp=0.9, mp=0.01, xmaxc=2.0, xmax_edge=50)
 
     print(c, weights)
     
     # executa o projeção novamente com os pesos que ajustaram a curva melhor
     graph.setWeights(city, c, weights)
-    graph.resetVertexValues()
-    # Ignorando o dicionário de predição para cada cidade.
-    prediction = graph.predict_cases(len(cities_new_cases[city])-1, city, debug=True)
+    
+    predictions = np.zeros(shape=(30,len(cities_new_cases[city])-1))
 
-    plt.plot(prediction, label="Prediction")
+    for i in range(30):
+        graph.resetVertexValues()
+        predictions[i] = graph.predict_cases(len(cities_new_cases[city])-1, city, debug=True)
+
+    mean_prediction = predictions.mean(axis=0)
+    
+    plt.plot(mean_prediction, label="Prediction")   
+    
+
     plt.plot(accumulated_curve, label="Real Curve")
     plt.grid()
     plt.xlabel("Qtde. Dias")
     plt.ylabel("Qtde. Casos")
     plt.xticks(list(range(0,len(accumulated_curve),10)))
     y_min = 0
-    y_max = max(accumulated_curve[-1], prediction[0])
-    plt.vlines(len(cities_new_cases[city])-1, ymin=y_min, ymax=y_max, colors='red',  linestyles='dashed', label='train/test')
-    plt.ylim([0,accumulated_curve[-1]])
+    y_max = max(accumulated_curve[-1], mean_prediction[len(cities_new_cases[city])-2])
+
+    plt.vlines(n_steps, ymin=y_min, ymax=y_max, colors='red',  linestyles='dashed', label='train/test')
+    plt.ylim([0,y_max])
     plt.legend()
     plt.savefig('plots/aprox.pdf')
