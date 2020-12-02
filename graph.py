bbf9069c11dea97a869bb8d3c506a335a2336bb5
Welton Santos <weltonsantos@dcc.ufmg.br>
2020-12-02 18:15:46 -0300

    Correção de erro na paralelização do código.
    No ramo single_city
    Your branch is up to date with 'origin/single_city'.
    
    Mudanças a serem submetidas:
    	modified:   grafos/grafo_tiradentes.txt
    	modified:   graph.py
    	modified:   main.py

--- graph.py
+++ graph.py
@@ -7,6 +7,7 @@ import random
 from datetime import datetime
 from random import random
 
+
 class Graph():
 
     def __init__(self, vertexes, edges, df, cities_new_cases):
@@ -18,31 +19,32 @@ class Graph():
 
         self.m = len(edges)
         self.n = len(vertexes)
-      
+
         self.graph.add_vertices(vertexes)
         self.graph.add_edges(edges)
-        
-        #define que o grafo é ponderado
+
+        # define que o grafo é ponderado
         self.graph.es["weight"] = 1.0
-        
-        self.dict_ibge_index = {}        
+
+        self.dict_ibge_index = {}
 
         for i in range(0, len(vertexes)):
             self.graph.vs[i]["name"] = df.iloc[i]['nome']
             self.graph.vs[i]["id"] = df.iloc[i]['cod_ibge']
             self.graph.vs[i]["lati"] = df.iloc[i]['lati']
             self.graph.vs[i]["long"] = df.iloc[i]['long']
-            self.graph.vs[i]["value"] = cities_new_cases[df.iloc[i]['cod_ibge']][0]
+            self.graph.vs[i]["value"] = cities_new_cases[df.iloc[i]
+                                                         ['cod_ibge']][0]
             self.dict_ibge_index[df.iloc[i]['cod_ibge']] = i
 
         # self.graph.save("grafos/grafo.gml", format="gml")
 
     def setWeights(self, ibge_id, c, weights):
-        # descobre o índice no grafo de uma determinada cidade        
+        # descobre o índice no grafo de uma determinada cidade
         index_current_vertex = self.dict_ibge_index[ibge_id]
 
         self.c = c
-                
+
         # altera apenas as arestas que incidem em um determinado vértice
         i = 0
         for edge in self.graph.get_edgelist():
@@ -52,22 +54,22 @@ class Graph():
 
             if i == len(weights):
                 break
-    
+
     # soma todos os novos casos de todas as cidades no passo corrente
-    def getTotalNewCases(self):        
+    def getTotalNewCases(self):
         sum = 0
         for v in self.graph.vs:
             sum += v['value']
 
         return sum
-        
-    #reseta o valor dos vértices para o valor inicial passado em cities_new_cases
+
+    # reseta o valor dos vértices para o valor inicial passado em cities_new_cases
     def resetVertexValues(self):
         for i in range(self.n):
             self.graph.vs[i]["value"] = self.cities_new_cases[self.graph.vs[i]['id']][0]
 
-    
     # avança um passo na predição
+
     def autoUpdateCases(self, step, ibge_id):
         newVertexesValue = np.zeros(shape=self.n)
         index_current_vertex = self.dict_ibge_index[ibge_id]
@@ -84,39 +86,41 @@ class Graph():
                             sum += 1
 
                 newVertexesValue[i] = self.graph.vs[i]['value'] * self.c + sum
-            
+
             else:
                 newVertexesValue[i] = self.cities_new_cases[self.graph.vs[i]["id"]][step]
 
         for i in range(self.n):
             self.graph.vs[i]["value"] = newVertexesValue[i]
 
-    
     # atualiza os novos casos n passos para a cidade ibge_id e retorna a curva acumulada
+
     def predict_cases(self, n_steps, ibge_id, debug=False):
-        
+
         def concat(parameters, sep):
             text = ''
             for i in range(len(parameters)):
                 text += str(parameters[i])
 
-                if i != len(parameters) -1:
+                if i != len(parameters) - 1:
                     text += sep
                 else:
-                    text +='\n'
+                    text += '\n'
 
             return text
-        
+
         def printVertexes(step, fileOut, vertexes):
             for vertex in vertexes:
-                fileOut.write(concat([step, vertex['id'], vertex['name'], vertex['value']], ','))
-        
+                fileOut.write(
+                    concat([step, vertex['id'], vertex['name'], vertex['value']], ','))
+
         if debug:
-            fileOut = open('logs/' + datetime.now().strftime("%Y-%m-%d_%H:%M:%S") + '.csv', 'w')
-            fileOut.write(concat(['step','ibge','name','newCases'], ','))
+            fileOut = open(
+                'logs/' + datetime.now().strftime("%Y-%m-%d_%H:%M:%S") + '.csv', 'w')
+            fileOut.write(concat(['step', 'ibge', 'name', 'newCases'], ','))
 
         cumulative_cases_city = []
-        
+
         sum = 0
         for i in range(n_steps):
 
@@ -127,8 +131,7 @@ class Graph():
                 printVertexes(i, fileOut, self.graph.vs)
 
             self.autoUpdateCases(i+1, ibge_id)
-            
-            
+
         if debug:
             fileOut.close()
 
