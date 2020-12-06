bf0dd1cbeafdff9f2e26366b424a415a9af78af2
Welton Santos <weltonsantos@dcc.ufmg.br>
2020-12-06 12:12:52 -0300

    Mudaças minimas.
    No ramo single_city
    Your branch is up to date with 'origin/single_city'.
    
    Mudanças a serem submetidas:
    	modified:   analise_resultados.ipynb
    	new file:   grafos/tiradentes_resize.png
    	modified:   model.py
    	modified:   plots/aprox_all.pdf
    	modified:   plots/melhor_elemento.pdf
    	new file:   plots/melhor_pior_media.pdf

--- model.py
+++ model.py
@@ -1,10 +1,8 @@
 from sys import argv, exit
 import sys
 sys.path.append('src')
-import os
 import pandas as pd
 import numpy as np
-import random
 from matplotlib import pyplot as plt
 from graph import Graph
 from utils import readFiles
@@ -31,34 +29,44 @@ if __name__=='__main__':
     # para cada um dos set de parâmetros.
     max_pred = 0
     df_sets = pd.read_csv('dados/best_set.csv')
-    for row in df_sets.itertuples():
-        
+    #labels = ['First','Second','Third', 'Fourth', 'Fifth','Sixth','Seventh','Eighth','Ninth','Tenth']
+    labels = ['Prediction']#,'Second','Third', 'Fourth', 'Fifth']
+    label_count = 0
+    plt.figure(figsize=(10,4))
+    #for row in df_sets.head(10).itertuples():
+    for row in df_sets.head(4).tail(1).itertuples():
         
         weights = np.array([ float(i) for i in row.weights.replace('[','').replace(']','').split() ])
         graph.setWeights(city, row.c, weights)
         
         predictions = np.zeros(shape=(30,len(cities_new_cases[city])-1))
 
-        for i in range(1):
+        for i in range(30):
             graph.resetVertexValues()
             predictions[i] = graph.predict_cases(len(cities_new_cases[city])-1, city, debug=True)
 
         mean_prediction = predictions.mean(axis=0)
+
+        with open('dados/pontos.txt', 'w') as fd:
+            fd.write(','.join([ str(i) for i in mean_prediction ]))
+
         if max_pred < mean_prediction[len(cities_new_cases[city])-2]:
             max_pred = mean_prediction[len(cities_new_cases[city])-2]
         
-        plt.plot(mean_prediction)
+        plt.plot(mean_prediction, label=labels[label_count], color="orange")
         
         plt.xlabel("Qtde. Dias")
         plt.ylabel("Qtde. Casos")
+        label_count += 1
     
-    plt.plot(accumulated_curve, label="Real Curve")
+    plt.plot(accumulated_curve, label="Real Curve", color="blue", linestyle="dashed")
     x = list(range(0,len(accumulated_curve),10))
     plt.xticks(x)
     y_min = 0
     y_max = max(accumulated_curve[-1], max_pred)
     plt.grid()
-    #plt.vlines(n_steps, ymin=y_min, ymax=y_max, colors='red',  linestyles='dashed', label='train/test')
+    plt.vlines(n_steps, ymin=y_min, ymax=y_max, colors='red',  linestyles='dashed', label='train/test')
     plt.ylim([0,y_max])
+    plt.xlim([0,156])
     plt.legend()
     plt.savefig('plots/aprox_all.pdf')
